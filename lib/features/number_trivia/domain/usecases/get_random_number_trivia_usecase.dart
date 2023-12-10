73dc6e3058bc69f8210c656192335bd074d91d8e
nich.otieno <nicholas.otieno@revca.tech>
2023-12-10 18:41:28 +0300

    Fix import statement in GetRandomNumberTriviaUsecase

--- lib/features/number_trivia/domain/usecases/get_random_number_trivia_usecase.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia_usecase.dart
@@ -1,8 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:number_trivia/core/error/failures.dart';
 import 'package:number_trivia/core/usecases/usecase.dart';
-import 'package:number_trivia/features/domain/entities/number_trivia.dart';
+
 import 'package:number_trivia/features/domain/repositories/number_trivia_repository.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 
 class GetRandomNumberTriviaUsecase with UseCases<NumberTrivia, NoParams> {
   final NumberTriviaRepository repository;
