94e74591dcc2871b2b3c0bb0358a75c3628c67ac
nich.otieno <nicholas.otieno@revca.tech>
2023-12-11 05:40:54 +0300

    update

--- test/features/number_trivia/data/datasources/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/datasources/models/number_trivia_model_test.dart
@@ -1,12 +1,31 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:number_trivia/features/number_trivia/data/datasources/models/number_trivia_model.dart';
 import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 
+import '../../../../../fixtures/fixture_reader.dart';
+
+
+
 void main() {
-  const tNumberTriviaModel = NumberTriviaModel(number:1,text:"test text");
+  const tNumberTriviaModel = NumberTriviaModel(number: 1, text: "test text");
 
   test('should be a subclass of NumberTrivia entity', () async {
     // assert
     expect(tNumberTriviaModel, isA<NumberTrivia>());
+  });
+
+  group("fromJson", () {
+    test('should return a valid when the Json number is an Integer', () async {
+      //arrange
+      final Map<String, dynamic> jsonMap = json.decode(fixture('trivia.json'));
+
+      //act
+      final result = NumberTriviaModel.fromJson(jsonMap);
+
+      //assert
+      expect(result, tNumberTriviaModel);
+    });
   });
 }
