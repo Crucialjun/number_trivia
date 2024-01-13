04963932f9cf94df379ad92b47e5afa914c9c6ee
nicholas.otieno <nicholas.otieno@revca.tech>
2024-01-13 17:26:37 +0300

    Add unit tests for getLastNumberTrivia() method

--- test/features/number_trivia/data/datasources/number_trivia_local_datasource_test.dart
+++ test/features/number_trivia/data/datasources/number_trivia_local_datasource_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_local_datasource.dart';
 
 import 'package:shared_preferences/shared_preferences.dart';
@@ -17,5 +18,30 @@ void main() {
         sharedPreferences: mockSharedPreferences);
   });
 
-  
+  group("getLastNumberTrivia", () {
+    test(
+        "should return NumberTrivia from SharedPreferences when there is one in the cache",
+        () async {
+      // arrange
+      when(mockSharedPreferences.getString(any))
+          .thenReturn('{"text": "test trivia", "number": 1}');
+      // act
+      final result = await dataSource.getLastNumberTrivia();
+      // assert
+      verify(mockSharedPreferences.getString(CACHED_NUMBER_TRIVIA));
+      expect(result.text, equals("test trivia"));
+      expect(result.number, equals(1));
+    });
+
+    test("should throw a CacheException when there is not a cached value",
+        () async {
+      // arrange
+      when(mockSharedPreferences.getString(any)).thenReturn(null);
+      // act
+      final call = dataSource.getLastNumberTrivia;
+      // assert
+      expect(() => call(), throwsA(isA<CacheException>()));
+    });
+  });
+  });
 }
