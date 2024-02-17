616420740b1f43d3f1c1b845af7b394e7754de1e
nich.otieno <nicholas.otieno@revca.tech>
2024-02-18 00:24:34 +0300

    Update dependencies in pubspec.yaml and fix imports in number_trivia_remote_datasource_test.dart

--- test/features/number_trivia/data/datasources/remote/number_trivia_remote_datasource_test.dart
+++ test/features/number_trivia/data/datasources/remote/number_trivia_remote_datasource_test.dart
@@ -1,11 +1,27 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 import 'package:mockito/annotations.dart';
+import 'package:number_trivia/features/number_trivia/data/datasources/remote/number_trivia_remote_datasource.dart';
+
+import 'number_trivia_remote_datasource_test.mocks.dart';
 
 
 
 @GenerateMocks([Dio])
 void main() {
+
+  late NumberTriviaRemoteDataSource dataSource;
+  late MockDio mockDio;
+
+  setUp(() {
+    mockDio = MockDio();
+    //dataSource = GetIt.instance<NumberTriviaRemoteDataSource>();
+  });
+
+
+
+
  
 
  
